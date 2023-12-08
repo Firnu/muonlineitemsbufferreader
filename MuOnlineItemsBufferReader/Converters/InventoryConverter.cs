897fbf24945b9aa7c73b2b158379771cb180f798
Marek Antoniuk <m.antoniuk@riftcat.com>
2023-12-08 08:59:43 +0100

    Poprawne formatowanie.

--- MuOnlineItemsBufferReader/Converters/InventoryConverter.cs
+++ MuOnlineItemsBufferReader/Converters/InventoryConverter.cs
@@ -17,41 +17,25 @@ namespace MuOnlineItemsBufferReader.Converters
 
             var itemSize = itemSizeUInt8 * 2;
 
-            var headlineSpacing = valueFormat switch
-            {
-                ValueFormat.Hex => "\t\t",
-                ValueFormat.Dec => "\t\t",
-                ValueFormat.Bit => "            ",
-                _ => throw new ArgumentOutOfRangeException(nameof(valueFormat), valueFormat, null)
-            };
-
-            var valueSpacing = valueFormat switch
-            {
-                ValueFormat.Hex => "\t\t",
-                ValueFormat.Dec => "\t\t",
-                ValueFormat.Bit => "\t",
-                _ => throw new ArgumentOutOfRangeException(nameof(valueFormat), valueFormat, null)
-            };
-
             var serialHeadline = valueFormat == ValueFormat.Bit ? "Serial" : "ItemSerial";
 
-            outputString.Append(
-                $"ID{headlineSpacing}" +
-                $"Lvl{headlineSpacing}" +
-                $"Dur.{headlineSpacing}" +
-                $"{serialHeadline}{headlineSpacing}" +
-                $"Exc{headlineSpacing}" +
-                $"Anc{headlineSpacing}" +
-                $"Seg{headlineSpacing}" +
-                $"Pink{headlineSpacing}" +
-                $"HrmSocO{headlineSpacing}" +
-                $"Soc1{headlineSpacing}" +
-                $"Soc2{headlineSpacing}" +
-                $"Soc3{headlineSpacing}" +
-                $"Soc4{headlineSpacing}" +
-                $"Soc5{headlineSpacing}" +
-                $"Other" +
-                Environment.NewLine);
+            var column = BuildColumnString();
+            outputString.Append(BuildColumnEntry(column, "ID"));
+            outputString.Append(BuildColumnEntry(column, "Lvl|Skl|Lck"));
+            outputString.Append(BuildColumnEntry(column, "Dur."));
+            outputString.Append(BuildColumnEntry(column, "Serial"));
+            outputString.Append(BuildColumnEntry(column, "Exc"));
+            outputString.Append(BuildColumnEntry(column, "Anc"));
+            outputString.Append(BuildColumnEntry(column, "Segment"));
+            outputString.Append(BuildColumnEntry(column, "Pink"));
+            outputString.Append(BuildColumnEntry(column, "Hrm|SOpt"));
+            outputString.Append(BuildColumnEntry(column, "Soc1"));
+            outputString.Append(BuildColumnEntry(column, "Soc2"));
+            outputString.Append(BuildColumnEntry(column, "Soc3"));
+            outputString.Append(BuildColumnEntry(column, "Soc4"));
+            outputString.Append(BuildColumnEntry(column, "Soc5"));
+            outputString.Append(BuildColumnEntry(column, "Other"));
+            outputString.Append(Environment.NewLine);
 
             for (var i = 0; i < input.Length; i += itemSize)
             {
@@ -60,32 +44,32 @@ namespace MuOnlineItemsBufferReader.Converters
                     outputString.Append(input.Substring(i, input.Length - i));
                     break;
                 }
-                
+
                 var rawLine = input.Substring(i, itemSize);
 
-                if(rawLine.All(q => q == 'F'))
+                if (rawLine.All(q => q == 'F'))
                 {
                     continue;
                 }
 
                 var buildLine = string.Empty;
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 8);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 1);
-                Reduce(ref buildLine, ref rawLine, 1);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, 2);
-                Reduce(ref buildLine, ref rawLine, rawLine.Length, true);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 8);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 1);
+                Reduce(ref rawLine, 1);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, 2);
+                Reduce(ref rawLine, rawLine.Length, true);
 
-                void Reduce(ref string builtLine, ref string raw, int shift, bool skipIndent = false)
+                void Reduce(ref string raw, int shift, bool lastEntry = false)
                 {
                     var value = raw[..shift];
 
@@ -96,21 +80,34 @@ namespace MuOnlineItemsBufferReader.Converters
                         ValueFormat.Bit => shift <= 2 ? Utils.ConvertHexToBitArray(value).PrintBits() : value,
                         _ => throw new ArgumentOutOfRangeException(nameof(valueFormat), valueFormat, null)
                     };
-                    
-                    builtLine += value;
 
-                    if (!skipIndent)
-                    {
-                        builtLine += valueSpacing;
-                    }
-                    
+                    var columnEntry = lastEntry ? value : BuildColumnEntry(column, value);
+                    outputString.Append(columnEntry); ;
                     raw = raw.Remove(0, shift);
                 }
-                
+
                 outputString.Append(buildLine + Environment.NewLine);
             }
 
             return outputString.ToString();
+        }
+
+        string BuildColumnEntry(string column, string entry)
+        {
+            return column.Remove(0, entry.Length).Insert(0, entry);
+        }
+
+        private string BuildColumnString()
+        {
+            var columnString = string.Empty;
+            const int columnSize = 12;
+
+            for (var i = 0; i < columnSize; i++)
+            {
+                columnString += " ";
+            }
+
+            return columnString;
         }
     }
 }
